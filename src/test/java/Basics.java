0dcfa435764d0d112956b06a76cb9f07be3307e2
Sritaj <sritajp@gmail.com>
2022-04-01 18:19:35 +0530

    Created API Test for Get method for assertion on Status Code

---
+++ src/test/java/Basics.java
@@ -0,0 +1,34 @@
+import io.restassured.RestAssured;
+
+import static io.restassured.RestAssured.given;
+
+public class Basics {
+
+    public static void main(String[] args) {
+
+        RestAssured.baseURI = "https://rahulshettyacademy.com";
+        given().queryParam("key", "qaclick123")
+                .header("Content-Type", "application/json")
+                .log().all()
+                .body("{\n" +
+                        "  \"location\": {\n" +
+                        "    \"lat\": -38.383494,\n" +
+                        "    \"lng\": 33.427362\n" +
+                        "  },\n" +
+                        "  \"accuracy\": 50,\n" +
+                        "  \"name\": \"Frontline house\",\n" +
+                        "  \"phone_number\": \"(+91) 983 893 3937\",\n" +
+                        "  \"address\": \"29, side layout, cohen 09\",\n" +
+                        "  \"types\": [\n" +
+                        "    \"shoe park\",\n" +
+                        "    \"shop\"\n" +
+                        "  ],\n" +
+                        "  \"website\": \"http://google.com\",\n" +
+                        "  \"language\": \"French-IN\"\n" +
+                        "}\n")
+                .when().post("/maps/api/place/add/json")
+                .then().log().all().assertThat().statusCode(200);
+
+    }
+
+}
