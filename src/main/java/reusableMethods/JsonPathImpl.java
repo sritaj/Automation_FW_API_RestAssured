67c965ea5528315400cc64780728c10d8b426416
Sritaj <sritajp@gmail.com>
2022-04-01 20:03:49 +0530

    Added JsonPath extraction methods under Reusable section

---
+++ src/main/java/reusableMethods/JsonPathImpl.java
@@ -0,0 +1,15 @@
+package reusableMethods;
+
+import io.restassured.path.json.JsonPath;
+
+public class JsonPathImpl {
+
+    public static JsonPath rawToJSON(String response) {
+        return new JsonPath(response);
+    }
+
+    public static String extractValueFromResponse(String response, String key) {
+        JsonPath jsonPath = new JsonPath(response);
+        return jsonPath.getString(key);
+    }
+}
