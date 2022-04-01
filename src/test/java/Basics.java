8fed2ad3a25e156d8067cb3426bcdda330e69783
Sritaj <sritajp@gmail.com>
2022-04-01 18:44:44 +0530

    Added Jsonpath to extract info from the response captured

--- src/test/java/Basics.java
+++ src/test/java/Basics.java
@@ -1,4 +1,5 @@
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
@@ -36,6 +37,12 @@ public class Basics {
                 .header("Server", "Apache/2.4.41 (Ubuntu)").extract().response().asString();
 
         System.out.println(response);
+
+        JsonPath jsonpath = new JsonPath(response);
+        String placeID = jsonpath.get("place_id");
+        System.out.println(placeID);
+
+
 
         //Validation of Add Place -> Get the place ID to update the address and validate it in the Response
     }
