a52a1177bb4bb574d2136fe79a3ab6befb6a4e91
Sritaj <sritajp@gmail.com>
2022-04-01 18:25:15 +0530

    Added Assertion for Body and Header

--- src/test/java/Basics.java
+++ src/test/java/Basics.java
@@ -1,6 +1,7 @@
 import io.restassured.RestAssured;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 public class Basics {
 
@@ -27,7 +28,8 @@ public class Basics {
                         "  \"language\": \"French-IN\"\n" +
                         "}\n")
                 .when().post("/maps/api/place/add/json")
-                .then().log().all().assertThat().statusCode(200);
+                .then().log().all().assertThat().statusCode(200).body("scope", equalTo("APP"))
+                .header("Server", "Apache/2.4.41 (Ubuntu)");
 
     }
 
