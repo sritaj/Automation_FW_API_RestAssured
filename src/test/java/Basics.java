c134d85896713a8e5428c68d6f90104d9390996a
Sritaj <sritajp@gmail.com>
2022-04-01 18:32:10 +0530

    Removed place from the Body and declared outside

--- src/test/java/Basics.java
+++ src/test/java/Basics.java
@@ -7,30 +7,35 @@ public class Basics {
 
     public static void main(String[] args) {
 
+        String place = "{\n" +
+                "  \"location\": {\n" +
+                "    \"lat\": -38.383494,\n" +
+                "    \"lng\": 33.427362\n" +
+                "  },\n" +
+                "  \"accuracy\": 50,\n" +
+                "  \"name\": \"Frontline house\",\n" +
+                "  \"phone_number\": \"(+91) 983 893 3937\",\n" +
+                "  \"address\": \"29, side layout, cohen 09\",\n" +
+                "  \"types\": [\n" +
+                "    \"shoe park\",\n" +
+                "    \"shop\"\n" +
+                "  ],\n" +
+                "  \"website\": \"http://google.com\",\n" +
+                "  \"language\": \"French-IN\"\n" +
+                "}\n";
+
+        //Validation of Add Place
+
         RestAssured.baseURI = "https://rahulshettyacademy.com";
         given().queryParam("key", "qaclick123")
                 .header("Content-Type", "application/json")
                 .log().all()
-                .body("{\n" +
-                        "  \"location\": {\n" +
-                        "    \"lat\": -38.383494,\n" +
-                        "    \"lng\": 33.427362\n" +
-                        "  },\n" +
-                        "  \"accuracy\": 50,\n" +
-                        "  \"name\": \"Frontline house\",\n" +
-                        "  \"phone_number\": \"(+91) 983 893 3937\",\n" +
-                        "  \"address\": \"29, side layout, cohen 09\",\n" +
-                        "  \"types\": [\n" +
-                        "    \"shoe park\",\n" +
-                        "    \"shop\"\n" +
-                        "  ],\n" +
-                        "  \"website\": \"http://google.com\",\n" +
-                        "  \"language\": \"French-IN\"\n" +
-                        "}\n")
+                .body(place)
                 .when().post("/maps/api/place/add/json")
                 .then().log().all().assertThat().statusCode(200).body("scope", equalTo("APP"))
                 .header("Server", "Apache/2.4.41 (Ubuntu)");
 
+        //Validation of Add Place -> Get the place ID to update the address and validate it in the Response
     }
 
 }
