4cc56a49e137aacb59b04fa687763e122e511d0f
Sritaj <sritajp@gmail.com>
2022-04-09 13:38:18 +0530

    Added PayPalProductPojo class

---
+++ src/main/java/pojo/PayPalProductPojo.java
@@ -0,0 +1,17 @@
+package pojo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PayPalProductPojo {
+
+    private String name;
+    private String description;
+    private String type;
+    private String category;
+    private String image_url;
+    private String home_url;
+
+}
