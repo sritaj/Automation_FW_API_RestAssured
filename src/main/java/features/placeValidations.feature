b1111b95cca96399026aadd6bc2cdb8268d96df2
Sritaj <sritajp@gmail.com>
2022-04-09 15:00:41 +0530

    Removed Cucumber related files/dependencies

--- src/main/java/features/placeValidations.feature
+++
@@ -1,6 +0,0 @@
-Feature: Validating Place API's
-
-  Scenario: Verify Add Place Functionality
-    Given I have Add Place Payload
-    When I call "AddPlaceAPI" with Post HTTP Request
-    Then I should get the Success Response with Status Code 200
--- src/test/java/cucumber/Options/TestRunner.java
+++
@@ -1,9 +0,0 @@
-package cucumber.Options;
-
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
-
-@CucumberOptions(features = "src/main/java/features", glue = {"stepDefinations"})
-public class TestRunner extends AbstractTestNGCucumberTests {
-
-}
