9551c11c372645a20fa00383800157e236e25fff
Sritaj <sritajp@gmail.com>
2022-04-11 01:45:40 +0530

    Added Plugin for "Allure Report Cucumber", "Maven-Cucumber-Report"

--- src/test/java/runner/CucumberRunner.java
+++ src/test/java/runner/CucumberRunner.java
@@ -5,7 +5,10 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(tags = "", features = "src/main/resources/features",
         glue = {"stepDefinations"}, dryRun = false,
-        plugin = {"pretty", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"})
+        plugin = {"pretty", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
+                "json:target/jsonReports/cucumber-report.json",
+                "html:target/jsonReports/cucumber.html",
+        })
 public class CucumberRunner extends AbstractTestNGCucumberTests {
 
 }
