16b793db63920a1ab621e71bd2d933d5b927d669
Sritaj <sritajp@gmail.com>
2022-04-11 01:01:00 +0530

    Added extentReport config under testRunner plugin

--- src/test/java/runner/CucumberRunner.java
+++ src/test/java/runner/CucumberRunner.java
@@ -3,7 +3,9 @@ package runner;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(tags = "", features = "src/main/resources/features", glue = {"stepDefinations"}, dryRun = false)
+@CucumberOptions(tags = "", features = "src/main/resources/features",
+        glue = {"stepDefinations"}, dryRun = false,
+        plugin = {"pretty", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"})
 public class CucumberRunner extends AbstractTestNGCucumberTests {
 
 }
