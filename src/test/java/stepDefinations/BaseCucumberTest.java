0246c388a85047e8b6dce7a9bb65a7f429a318ac
Sritaj <sritajp@gmail.com>
2022-04-11 12:50:10 +0530

    Updated BasePath and BaseURI using properties instead of hardcoded val

--- src/test/java/stepDefinations/BaseCucumberTest.java
+++ src/test/java/stepDefinations/BaseCucumberTest.java
@@ -1,13 +1,15 @@
 package stepDefinations;
 
+import enums.ReqResSpecs;
 import io.cucumber.java.Before;
 import io.restassured.RestAssured;
+import utilities.PropertiesFileImpl;
 
 public class BaseCucumberTest {
 
     @Before
     public void setup() {
-        RestAssured.baseURI = "https://reqres.in";
-        RestAssured.basePath = "/api";
+        RestAssured.baseURI = PropertiesFileImpl.getDataFromPropertyFile(ReqResSpecs.REQRESURI);
+        RestAssured.basePath = PropertiesFileImpl.getDataFromPropertyFile(ReqResSpecs.REQRESBASEPATH);
     }
 }
